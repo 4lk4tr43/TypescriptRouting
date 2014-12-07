00b36987f653a60ce6ed491cd268a27ca55abb01
4lk4tr43 <4lk4tr43@gmail.com>
2014-12-08 00:27:32 +0100

    Gulp adjusted to use shell, divided into 2 task (windows bug)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp'),
     typescript = require('gulp-tsc'),
+    shell = require('gulp-shell'),
     uglify = require('gulp-uglifyjs');
 
 gulp.task('Build library', function () {
-   gulp.src('scripts/Routing/Annotations/route.ts')
-       .pipe(typescript({ target: 'ES5', out: 'routing.js'}))
-       .pipe(uglify('routing.js', { mangle: true }))
-       .pipe(gulp.dest(''))
+   gulp.src('scripts/Routing/Annotations/route.ts', {read: false})
+       .pipe(shell([
+           'tsc <%= file.path %> --out routing.js'
+       ]));
 });
 
-gulp.task('Minify library', function () {
+gulp.task('Minimize library', function () {
     gulp.src('routing.js')
         .pipe(uglify('routing.js', { mangle: true }))
         .pipe(gulp.dest(''))
